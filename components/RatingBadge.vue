ec6c19303be5d012e01b48c658a36dd78d429478
Florencia Rímolo <florencia.rf96@gmail.com>
2026-01-07 17:24:17 +0100

    Update RatingBadge component to dynamically change colors based on rating value
    - Modified the RatingBadge.vue to use conditional classes for background and text colors based on the rating value.
    - Replaced the TMDB logo with a star icon for improved visual representation.
    - Enhanced the template comments for better clarity on the component's functionality.

--- components/RatingBadge.vue
+++ components/RatingBadge.vue
@@ -1,9 +1,19 @@
 <template>
-  <!-- Rating badge with TMDB logo -->
+  <!-- Rating badge, with different color according the value -->
   <div
-    class="text-xs font-medium whitespace-nowrap flex items-center gap-1 flex-shrink-0 min-w-fit"
+    :class="{
+      'bg-green-600/90 dark:bg-green-700/90 border-green-500/30 text-green-300':
+        rating >= 7,
+      'bg-yellow-600/90 dark:bg-yellow-500/90 border-yellow-500/30 text-yellow-300':
+        rating >= 5 && rating < 7,
+      'bg-gray-600/90 dark:bg-gray-500/90 border-gray-500/30 text-gray-300':
+        rating === 0 || !rating,
+      'bg-red-600/90 dark:bg-red-500/90 border-red-500/30 text-red-300':
+        rating && rating < 5,
+    }"
+    class="px-2 py-1.5 text-xs font-medium rounded-full shadow-sm shadow-black/50 border whitespace-nowrap flex items-center gap-1 flex-shrink-0 min-w-fit"
   >
-    <img src="/tmdb-logo.svg" alt="TMDB" class="w-10 h-10 flex-shrink-0" />
+    <IconStar icon-class="w-3 h-3" />
     {{
       rating?.toFixed(1) === '0.0' || rating === 0 || !rating
         ? 'N/A'
@@ -13,6 +23,8 @@
 </template>
 
 <script setup lang="ts">
+import IconStar from '@/components/icons/IconStar.vue';
+
 defineProps({
   rating: {
     type: Number,
